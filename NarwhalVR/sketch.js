afa549dd63b93ba882aaad7bb08f57c3732cd0b9
redmoe <31392444+redmoe@users.noreply.github.com>
2018-04-26 17:52:30 -0400

    Final Version 2.1

--- NarwhalVR/sketch.js
+++ NarwhalVR/sketch.js
@@ -1,4 +1,5 @@
 var narwhals = 0;
+var narwhalNames = ["Clive","Alfred","Maxwell","Ainsley"];
 
 // function loop() {
 // 	var sceneEl = document.querySelector('a-scene');
@@ -19,16 +20,37 @@ var narwhals = 0;
 // 	});
 //   }
 // });
+
 var f = 0;
 for (var i = 0; i < 4; i++) {
-  AFRAME.registerComponent('modely'+i, {
+
+    var sceneEl = document.querySelector('a-scene');
+    var entity = document.createElement('a-entity');
+    var newEntity = sceneEl.appendChild(entity);
+    // newEntity.object3D.position.set(-20,10,i*10-20);
+    // newEntity.object3D.rotation.set(0,0,180);
+    newEntity.setAttribute("id","modely"+i);
+    newEntity.setAttribute("modely"+i);
+    newEntity.setAttribute("gltf-model","#narwhal");
+    newEntity.setAttribute("name",narwhalNames[i%narwhalNames.length]);
+    var animation = document.createElement('a-animation');
+    animation.setAttribute("attribute","rotation");
+    animation.setAttribute("to","0 360 0");
+    animation.setAttribute("repeat","indefinite");
+    animation.setAttribute("fill","forwards");
+        animation.setAttribute("duration", "1000");
+
+    newEntity.appendChild(animation);
+
+        console.log(animation);
+
+    AFRAME.registerComponent('modely'+i, {
         init: function() {
           var data = this.data;
           var el = this.el;
-          el.object3D.position.set(-20,10,f*10-20);
-                        el.object3D.rotation.set(0,0,180);
-
-          f++;
+         el.object3D.position.set(-20,4,f*10-20);
+         // el.object3D.rotation.set(0,0,180);
+         f++;
           var pressTimer = null;
           var sizeTimer = null;
           var longpress = false;
@@ -39,31 +61,31 @@ for (var i = 0; i < 4; i++) {
               clearInterval(sizeTimer);
               pressTimer = null;
             }
-
             if (longpress) {
               return false;
             }
-
             console.log('mouse up');
           }); 
           
-            el.addEventListener('mouseenter', function(e) {
+          el.addEventListener('mouseenter', function(e) {
             console.log("mouse down");
             longpress = false;
             sizeTimer = setInterval(function(){   
-
-              el.object3D.scale.set(el.object3D.scale.x/1.1,el.object3D.scale.y/1.1,el.object3D.scale.z/1.1);
+              //el.object3D.scale.set(el.object3D.scale.x/1.1,el.object3D.scale.y/1.1,el.object3D.scale.z/1.1);
             }, 100);
 
             pressTimer = setTimeout(function(){
               console.log("long click");
               narwhals++;
+              //narwhalNames.push(el.getAttribute('name'));
+              var opedia = sceneEl.querySelector('#Narwhalopedia');
+              //console.log(opedia);
+              opedia.setAttribute('value',opedia.getAttribute('value') + el.getAttribute('name') + ", ");
               var narwhal = sceneEl.querySelector('#'+el.getAttribute('id'));
               narwhal.sceneEl.removeChild(narwhal);
               sceneEl.querySelector('#UItext').setAttribute('value',"Narwhal Count: " + narwhals);
               longpress = true;
             },2000);
-
           });
       }
   });
